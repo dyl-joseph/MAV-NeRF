cf040d645a7128a28d8aebed1f3c3dbfe796002e
= <=>
2025-07-12 23:46:25 -0700

    updates

--- data.py
+++ data.py
@@ -1,12 +1,31 @@
-from test_replay_dataset import TestReplayDataset
+from replay_dataset import ReplayDataset
 
-# Create an instance of TestReplayDataset
-dataset = TestReplayDataset()
+SQL_ROOT = r"C:\Users\dylan\replay_dataset\tests\data\replay_dataset_100.sqlite"
+NO_BLOBS_KWARGS = {
+    "dataset_root": "",
+    "load_images": False,
+    "load_depths": False,
+    "load_masks": False,
+    "load_depth_masks": False,
+    "box_crop": False,
+}
 
-# Optionally, check the length of the dataset
-# print("Dataset length:", len(dataset))
 
-# Access and print the first few items
-for i in range(min(0,5)):
+
+dataset = ReplayDataset(
+    sqlite_metadata_file=SQL_ROOT,
+    remove_empty_masks=False,
+    frame_data_builder_ReplayFrameDataBuilder_args=NO_BLOBS_KWARGS,
+)
+
+print(type(dataset))
+print(len(dataset))
+for i in range(len(dataset)):
     item = dataset[i]
-    print(f"Item {i}:", item)
+    print(item.sequence_name)
+
+sequence = 'cat0_seq0'
+frame_number = 0
+item = dataset[sequence, frame_number]
+print(item)
+    
